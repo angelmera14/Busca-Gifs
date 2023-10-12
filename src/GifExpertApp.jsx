0269e37b7f7e63a033f672a13cfa8a7e9bb111c1
Angel Mera Delgado <angelmeradelgado@gmail.com>
2023-10-11 21:18:26 -0500

    Subiendo Aplicacion

---
+++ src/GifExpertApp.jsx
@@ -0,0 +1,35 @@
+import { useState } from 'react'
+import { AddCategory, GifGrid } from "./components";
+
+export const GifExpertApp = ()=>{
+
+    const [categoria, setCategoria] = useState([''])
+
+     const onAddCategory = ( newCategory ) => {
+
+        if ( categoria.includes(newCategory)) return;
+
+         setCategoria([...categoria, newCategory])
+        //  setCategoria(cats => [...cats,'HunterXHunter'] )
+        }
+
+    return (
+        <>
+            <h1>Encuentra los mejores Gifs del mundo</h1>
+
+            <AddCategory 
+                onNewCategory = {event => onAddCategory(event)}
+            />
+            {
+                categoria.map( category =>(
+                    <GifGrid 
+                        key = {category}
+                        category={category} />
+                        
+                ))
+            }
+       
+        </>
+    )
+
+}
